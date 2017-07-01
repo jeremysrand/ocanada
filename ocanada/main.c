ccab57df817355db994cadc862be7307ef4f3c6a
Jeremy Rand <jeremy@rand-family.com>
2017-07-01 18:45:21 -0400

    Initial Commit

---
+++ ocanada/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ * ocanada
+ *
+ * Created by Jeremy Rand on 2017-07-01.
+ * Copyright (c) 2017 Jeremy Rand. All rights reserved.
+ *
+ */
+
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(void)
+{
+    printf("HELLO, WORLD!\n");
+    cgetc();
+    return 0;
+}
